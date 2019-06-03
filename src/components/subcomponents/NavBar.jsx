b30d944975b7606f207154e297ba69bae966d4b9
pacokwon <haechank@gmail.com>
2019-06-03 16:39:13 +0900

    removed irrelevant svg icon

--- src/components/subcomponents/NavBar.jsx
+++ src/components/subcomponents/NavBar.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from '@material-ui/core/Typography'
-import SvgIcon from '@material-ui/core/SvgIcon'
-import { ReactComponent as Logo } from '../../assets/Symbol_white.svg'
 
 const NavBar = ({ title }) => {
     return (
@@ -13,9 +11,6 @@ const NavBar = ({ title }) => {
                     <Typography variant="h6" color="inherit">
                         {title}
                     </Typography>
-                    <SvgIcon>
-                        <img src={Logo} alt="" />
-                    </SvgIcon>
                 </Toolbar>
             </AppBar>
         </div>
